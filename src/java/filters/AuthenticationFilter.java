17d77c2ec2052d356cc779a78105b590a64ab63e
730676 <730676@SAIT224439IT.ACDM.DS.SAIT.CA>
2020-11-11 17:25:57 -0700

    Lab 10

--- src/java/filters/AuthenticationFilter.java
+++ src/java/filters/AuthenticationFilter.java
@@ -41,12 +41,12 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void destroy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //To change body of generated methods, choose Tools | Templates.
     }
        
 }
